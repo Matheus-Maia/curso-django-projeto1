db44cb880bed0c38e62cc80bf2290032a7221a9e
Matheus-Maia <matheusmaiadev535@gmail.com>
2025-06-02 10:16:55 -0300

    Add templates de contato, home

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,12 @@
 from django.http import HttpResponse
-#from django.shortcuts import render
+from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Home")
+    return render(request, 'recipes/home.html')
 
 def contato(request):
-    return HttpResponse("Contato")
+    return render(request, 'recipes/contato.html')
 
 def sobre(request):
     return HttpResponse("Sobre") 
