00400fb2f5d9c53c04e0fb217ec35796af527e76
Matheus-Maia <matheusmaiadev535@gmail.com>
2025-06-03 13:29:22 -0300

    Add context to home and recipes views to display generated recipes

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipes
 
 # Create your views here.
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={'recipes': [make_recipes() for _ in range(10)]}) # < gera 10 receitas com a fabrica
 
 def recipes(request, id):
-    return render(request, 'recipes/pages/recipe-view.html')
+    return render(request, 'recipes/pages/recipe-view.html', context={'recipe': make_recipes()})
